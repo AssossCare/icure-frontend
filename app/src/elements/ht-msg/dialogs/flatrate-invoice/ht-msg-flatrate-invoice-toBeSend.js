a12900b926076f738c54f3fcd8a307fbace237c4
LAPSAM5\Sam Jocqué <sam@figac.be>
2020-06-18 09:29:30 +0200

    WIP: re-enable invoice generation (working, gets to finally without errors)

--- app/src/elements/ht-msg/dialogs/flatrate-invoice/ht-msg-flatrate-invoice-toBeSend.js
+++ app/src/elements/ht-msg/dialogs/flatrate-invoice/ht-msg-flatrate-invoice-toBeSend.js
@@ -7,6 +7,8 @@ import '../../../../styles/buttons-style'
 import '../../../../styles/dialog-style'
 import '../../../../styles/invoicing-style';
 import '../../../ht-spinner/ht-spinner.js'
+import '../../../ht-pat/dialogs/medicalhouse/ht-pat-flatrate-utils.js';
+
 
 //TODO import "@polymer/iron-collapse-button/iron-collapse-button"
 import "@polymer/iron-icon/iron-icon"
@@ -418,6 +420,8 @@ class HtMsgFlatrateInvoiceToBeSend extends TkLocalizerMixin(PolymerElement) {
                 </div>     
             </div>        
         </paper-dialog>
+        
+        <ht-pat-flatrate-utils id="flatrateUtils" api="[[api]]" user="[[user]]" language="[[language]]" patient="[[patient]]" i18n="[[i18n]]" current-contact="[[currentContact]]" i18n="[[i18n]]" resources="[[resources]]" no-print></ht-pat-flatrate-utils>
 `
     }
 
@@ -489,6 +493,10 @@ class HtMsgFlatrateInvoiceToBeSend extends TkLocalizerMixin(PolymerElement) {
                 value: false
             },
             progressItem:{
+                type: Array,
+                value: () => []
+            },
+            _loadingMessages: {
                 type: Array,
                 value: () => []
             }
@@ -1314,7 +1322,8 @@ class HtMsgFlatrateInvoiceToBeSend extends TkLocalizerMixin(PolymerElement) {
                 )
             }) //22
             .finally(()=>{
-                console.log(this.flatRateInvoicingDataObject);
+                console.log("finally:flatRateInvoicingDataObject", this.flatRateInvoicingDataObject);
+                console.log(JSON.stringify(this.flatRateInvoicingDataObject));
                 this.flatRateInvoicingDataObject = {}
                 this.set('isLoading', false );
                 this.set('activeGridItem', null );
@@ -1408,6 +1417,14 @@ class HtMsgFlatrateInvoiceToBeSend extends TkLocalizerMixin(PolymerElement) {
         return parseInt(moment().format('YYYY'))
     }
 
+    _sleep (time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+    getChangeParentCode306(code){
+        return code === "306" ? "300" : code;
+    }
+
     _setLoadingMessage( messageData ) {
         if( messageData.updateLastMessage ) { this._loadingMessages.pop(); }
         this._loadingMessages.push( messageData );
@@ -1417,6 +1434,203 @@ class HtMsgFlatrateInvoiceToBeSend extends TkLocalizerMixin(PolymerElement) {
 
     _resetLoadingMessage() {
         this._loadingMessages = [];
+    }
+
+    _getPatientsByHcp( hcpId ) {
+
+        return this.api.getRowsUsingPagination(
+            (key,docId) =>
+                this.api.patient().listPatientsByHcPartyWithUser(this.user, hcpId, null, key && JSON.stringify(key), docId, 1000)
+                    .then(pl => {
+                        pl.rows = _
+                            .chain(pl.rows)
+                            .filter((i)=>{return(
+                                !!i
+                                && !!_.get(i,"active", true)
+                                && !!_.trim(_.get(i,"dateOfBirth", ""))
+                                && !!_.trim(_.get(i,"ssin", ""))
+                                && !!_.size(_.get(i,"insurabilities", []))
+
+                                // Make sure there is at least one valid insurance versus exported date
+                                && !!_.size(
+                                    _.chain(i.insurabilities)
+                                        .filter(i=>{return(
+                                            !!i
+                                            // && i.identificationNumber
+                                            && !!_.trim(_.get(i,"insuranceId", ""))
+                                            && _.trim(_.get(i, "parameters.tc1", "")).length === 3
+                                            && _.trim(_.get(i, "parameters.tc2", "")).length === 3
+                                            && ( _.trim(_.get(i, "parameters.tc1", "")) + _.trim(_.get(i, "parameters.tc2", "")) !== "000000" )
+                                            && (
+                                                moment(_.get(i, "startDate"+"", 0), 'YYYYMMDD').isBefore(this.reportCurrentDateMomentObject, 'date') ||
+                                                moment(_.get(i, "startDate"+"", 0), 'YYYYMMDD').isSame(this.reportCurrentDateMomentObject, 'date') ||
+                                                !parseInt(_.get(i, "startDate", 0))
+                                            )
+                                            && (
+                                                moment(_.get(i, "endDate"+"", 0), 'YYYYMMDD').isAfter(this.reportCurrentDateMomentObject, 'date') ||
+                                                moment(_.get(i, "endDate"+"", 0), 'YYYYMMDD').isSame(this.reportCurrentDateMomentObject, 'date') ||
+                                                !parseInt(_.get(i, "endDate", 0))
+                                            )
+                                        )})
+                                        .value()
+                                )
+                                && !!_.size(_.get(i, "medicalHouseContracts", []))
+                                && !!_.size(_.filter(_.get(i, "medicalHouseContracts",[]), i => _.trim(_.get(i,"hcpId", "something")) === _.trim(_.get(this,"user.healthcarePartyId","else"))))
+                            )})
+                            .uniqBy( 'ssin' )
+                            .value()
+                            .map((i) => {
+                                i.ssin = _.trim(_.get(i,"ssin","")).replace(/[^\d]/gmi,"")
+                                i.lastName = (_.get(i,"lastName","")).toUpperCase()
+                                i.firstName = (_.get(i,"firstName","")).toUpperCase()
+                                i.dateOfBirth = (!!_.trim(_.get(i,"dateOfBirth",""))?moment(_.trim(_.get(i,"dateOfBirth",0)), "YYYYMMDD").format('DD/MM/YYYY'):"")
+
+                                // Eval "finalInsurability" to be the one corresponding to exported date
+                                i.finalInsurability = _.get(
+                                    _.filter(
+                                        i.insurabilities,
+                                        (ins) => {
+                                            return ins &&
+                                                _.size(ins) &&
+                                                !!_.trim(_.get( ins, "insuranceId", "" )) &&
+                                                _.trim(_.get(ins, "parameters.tc1", "")).length === 3 &&
+                                                _.trim(_.get(ins, "parameters.tc2", "")).length === 3 &&
+                                                ( _.trim(_.get(ins, "parameters.tc1", "")) + _.trim(_.get(ins, "parameters.tc2", "")) !== "000000" ) &&
+                                                // !!_.trim(_.get( ins, "identificationNumber", "" ) ) &&
+                                                (
+                                                    moment(_.get(ins, "startDate"+"", 0), 'YYYYMMDD').isBefore(this.reportCurrentDateMomentObject, 'date') ||
+                                                    moment(_.get(ins, "startDate"+"", 0), 'YYYYMMDD').isSame(this.reportCurrentDateMomentObject, 'date') ||
+                                                    !parseInt(_.get(ins, "startDate", 0))
+                                                ) &&
+                                                (
+                                                    moment(_.get(ins, "endDate"+"", 0), 'YYYYMMDD').isAfter(this.reportCurrentDateMomentObject, 'date') ||
+                                                    moment(_.get(ins, "endDate"+"", 0), 'YYYYMMDD').isSame(this.reportCurrentDateMomentObject, 'date') ||
+                                                    !parseInt(_.get(ins, "endDate", 0))
+                                                )
+                                        }
+                                    ), "[0]", {}
+                                )
+                                i.insurancePersonType = !_.trim( _.get( i, "finalInsurability.titularyId", "" )) ? "T" : ( moment().diff(moment(_.get(i, "dateOfBirth"+"", "0")+"", "DD/MM/YYYY"), 'years') < 18 ) ? "E" : "C"
+                                i.titularyId = _.trim( _.get( i, "finalInsurability.titularyId", "" ))
+                                return i
+                            })
+                        ;
+                        return {
+                            rows:pl.rows,
+                            nextKey: pl.nextKeyPair && pl.nextKeyPair.startKey,
+                            nextDocId: pl.nextKeyPair && pl.nextKeyPair.startKeyDocId,
+                            done: !pl.nextKeyPair
+                        }
+                    })
+                    .catch(()=>{ return Promise.resolve(); })
+        )||[];
+
+    }
+
+    _getPatientsWithValidInvoiceForExportedMonth(patients, exportedMonth) {
+
+        let prom = Promise.resolve([])
+
+        _.map(patients, pat => {
+            prom = prom.then(promisesCarrier => this.api.invoice().findBy(_.get(this,"user.healthcarePartyId"), pat)
+                .then(invoices => _.filter(invoices, inv => inv &&
+                    inv.sentDate &&
+                    inv.sentMediumType === "cdrom" &&
+                    _.size(_.filter(inv.invoicingCodes, ic => !ic.lost && !ic.canceled && !ic.resent)) &&
+                    _.trim(inv.invoiceDate) === exportedMonth
+                ))
+                .then(patValidInvoicesForExportedMonth => !!_.size(patValidInvoicesForExportedMonth) ? _.get(pat,"id",false) : false)
+                .then(patId => _.concat(promisesCarrier, [patId]))
+                .catch(()=>_.concat(promisesCarrier, [false]))
+            )
+        })
+
+        return prom.then(x=>_.compact(x))
+
+    }
+
+    getDestCode(affCode){
+        let destCode = affCode;
+        if (affCode.startsWith("3")) {
+            if (["304", "305", "309", "311", "315", "317", "319", "322", "323", "325"].includes(affCode)) {
+                destCode = "300";
+            } else {
+                destCode ="306";
+            }
+        } else if (affCode.startsWith("4")) {
+            destCode =  "400"
+        }
+        return  destCode;
+    }
+
+    _getInsuranceTitularyInfo(inputPatientsList=false) {
+        return new Promise(resolve =>{
+            const insuranceTitularyIds = _.compact(_.uniq(_.filter( (inputPatientsList?inputPatientsList:this.flatRateAllPatients), (i)=>{ return _.trim(_.get(i, "titularyId", "")) }).map(i=>i.titularyId) ));
+            return !_.size(insuranceTitularyIds) ? resolve((inputPatientsList?inputPatientsList:this.flatRateAllPatients)) : this.api.patient().getPatientsWithUser(this.user, new models.ListOfIdsDto({ ids: insuranceTitularyIds })).then(results => {
+                //this.api.setPreventLogging(false)
+                return resolve(
+                    _.map((inputPatientsList?inputPatientsList:this.flatRateAllPatients), (i=>{
+                        if(!_.trim(_.get(i,"titularyId", "" ))) return i
+                        let titularyRecord = _.head(_.filter(results,(j=>{ return _.trim(j.id) === _.get(i,"titularyId", "" ) })))
+                        i.titularyLabel = _.upperCase(_.get(titularyRecord, "firstName", "" )) + ' ' + _.upperCase(_.get(titularyRecord, "lastName", "" ))
+                        return i
+                    }))
+                )
+            })
+        })
+    }
+
+    _getInsurancesDataByPatientsList(inputPatientsList) {
+        return new Promise(resolve => {
+            this.api.insurance().getInsurances(new models.ListOfIdsDto({ids : _.chain(inputPatientsList).map(i=> i.insurabilities.map(ins => _.trim(_.get(ins, "insuranceId")))).flattenDeep().uniq().compact().value()}))
+                .then(insurancesData => resolve(
+                    _
+                        .chain(insurancesData)
+                        .map((i)=>{ i.finalName = (i && i.name && i.name[this.language]) ? i.name[this.language] : i.name[(this.language==='fr' ? 'nl' : 'fr')]; return i; })
+                        .sortBy((i)=>{ return i.code; })
+                        .value()
+                ))
+        })
+    }
+
+    _getInsurancesDataByIds(insurancesIds) {
+        return new Promise(resolve => {
+            this.api.insurance().getInsurances(new models.ListOfIdsDto({ids : insurancesIds}))
+                .then(insurancesData => resolve(
+                    _
+                        .chain(insurancesData)
+                        .map((i)=>{ i.finalName = (i && i.name && i.name[this.language]) ? i.name[this.language] : i.name[(this.language==='fr' ? 'nl' : 'fr')]; return i; })
+                        .sortBy((i)=>{ return i.code; })
+                        .value()
+                ))
+        })
+    }
+
+    _getInsurancesDataByCode(insurancesCode) {
+        return new Promise(resolve => {
+            this.api.insurance().listInsurancesByCode(insurancesCode)
+                .then(insurancesData => resolve(
+                    _
+                        .chain(insurancesData)
+                        .map((i)=>{ i.finalName = (i && i.name && i.name[this.language]) ? i.name[this.language] : i.name[(this.language==='fr' ? 'nl' : 'fr')]; return i; })
+                        .sortBy((i)=>{ return i.code; })
+                        .value()
+                ))
+        })
+    }
+
+    _getIOsDataByInsurancesList(inputInsurancesList) {
+
+        return new Promise(resolve => {
+            this.api.insurance().getInsurances(new models.ListOfIdsDto({ids : _.chain(inputInsurancesList).map(i=>_.trim(_.get(i, "parent"))).uniq().compact().value()}))
+                .then(insurancesData => resolve(
+                    _
+                        .chain(insurancesData)
+                        .map((i)=>{ i.finalName = (i && i.name && i.name[this.language]) ? i.name[this.language] : i.name[(this.language==='fr' ? 'nl' : 'fr')]; return i; })
+                        .sortBy((i)=>{ return i.code; })
+                        .value()
+                ))
+        })
     }
 }
 
